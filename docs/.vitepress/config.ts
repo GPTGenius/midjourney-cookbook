a1bbfd5ebec1993061ff38d1b097f202f433b291
fatdoge <704274491@qq.com>
2023-05-16 19:22:19 +0800

    fix

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Midjourney Prompts && Cases",
   description: "More Magic Magination",
+  base: "/midjourney-cookbook/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
