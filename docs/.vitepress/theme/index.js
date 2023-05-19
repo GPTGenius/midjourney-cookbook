478a29a864a82d49248a5541da5e3fe8eaa271ae
lvqq <nicksonlvqq@gmail.com>
2023-05-19 22:13:45 +0800

    fix: image zoom not work after route changes

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,13 +1,21 @@
 import DefaultTheme from 'vitepress/theme';
-import { onMounted } from 'vue';
+import { onMounted, watch, nextTick } from 'vue';
 import mediumZoom from 'medium-zoom';
+import { useRoute } from 'vitepress';
 import './index.css';
 
 export default {
   ...DefaultTheme,
   setup() {
-    onMounted(() => {
+    const route = useRoute();
+    const initZoom = () =>
       mediumZoom('.main img', { background: 'var(--vp-c-bg)' });
+    onMounted(() => {
+      initZoom();
     });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
   },
 };
